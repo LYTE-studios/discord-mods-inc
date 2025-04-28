235a447c34197072aeea0efc3f1fc3b2a90f7e9f
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2025-04-28 16:32:21 +0200

    f

--- web/chat/urls.py
+++ web/chat/urls.py
@@ -9,5 +9,7 @@ app_name = 'chat'
 
 urlpatterns = [
     path('', views.ChatListView.as_view(), name='list'),
+    path('room/', views.ChatRoomView.as_view(), name='chat_room'),
+    path('room/<int:pk>/', views.ChatRoomView.as_view(), name='chat_room'),
     path('api/', include(router.urls)),
 ]
--- web/chat/views.py
+++ web/chat/views.py
@@ -4,14 +4,51 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
+
 
 class ChatListView(LoginRequiredMixin, TemplateView):
     """
     View for displaying the chat list page.
     """
     template_name = 'chat/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['conversations'] = Conversation.objects.filter(user=self.request.user)
+        return context
+
+class ChatRoomView(LoginRequiredMixin, TemplateView):
+    """
+    View for displaying the chat room page.
+    Can be used for both new conversations and existing ones.
+    """
+    template_name = 'chat/room.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pk = self.kwargs.get('pk')
+        
+        if pk:
+            # Existing conversation
+            conversation = get_object_or_404(Conversation, pk=pk, user=self.request.user)
+            context['conversation'] = conversation
+            context['messages'] = conversation.messages.all().order_by('created_at')
+        else:
+            # New conversation
+            context['conversation'] = None
+            context['messages'] = []
+            
+        # Add WebSocket URL for chat
+        ws_protocol = 'wss' if self.request.is_secure() else 'ws'
+        ws_host = self.request.get_host()
+        context['websocket_url'] = f"{ws_protocol}://{ws_host}/ws/chat/"
+        if pk:
+            context['websocket_url'] += f"{pk}/"
+            
+        return context
 
 class ConversationViewSet(viewsets.ModelViewSet):
     """
