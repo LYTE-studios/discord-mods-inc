6071e190efcb7fffa434028b1dba6ea6d1fe70dd
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2025-04-28 16:26:18 +0200

    url

--- web/users/urls.py
+++ web/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from rest_framework.routers import DefaultRouter
 from . import views
 
@@ -9,5 +10,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include(router.urls)),
 ]
